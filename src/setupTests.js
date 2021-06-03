8356183520e703b8ed11e09948e5da37ffc1fe7c
Michael Greenwald <mgreenwaldm@icloud.com>
2021-06-03 18:43:49 -0500

    Mvp

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,5 +2,14 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
-import 'mutationobserver-shim';
+
+import React from 'react';
+import {render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import ContactForm from './ContactForm';
+
+test('renders without errors', ()=>{
+
+});
